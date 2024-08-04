3fe83ec416bb01943d6209b81730802e5920b329
Deodate <ajadeo04@gmail.com>
2024-08-04 02:50:27 +0200

    Progress

--- src/app/igihe/igihe.component.ts
+++ src/app/igihe/igihe.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { GET_NEWS } from '../graphql.operations';
 
 @Component({
   selector: 'app-igihe',
   templateUrl: './igihe.component.html',
   styleUrls: ['./igihe.component.css']
 })
-export class IgiheComponent {
+export class IgiheComponent implements OnInit {
 
+  news: any[] = [];
+  error: any;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit(): void {
+    this.apollo.watchQuery({
+      query: GET_NEWS
+    }).valueChanges.subscribe(({data, error }: any) => {
+      this.news = data.news;
+      this.error = error
+    }
+    );
+     
+  }
 }
